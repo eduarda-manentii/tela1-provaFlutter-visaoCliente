4751fe8ae4a028a45f24b3195af69a65afa50cf2
Eduarda Manenti <eduarda_manenti@nuntec.com.br>
2023-05-30 13:19:42 -0300

    criado o método middle_buttons para organizar melhor a tela principal.

--- lib/componentes/middle_buttons.dart
+++ lib/componentes/middle_buttons.dart
@@ -1,5 +1,9 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+
+import 'newsletter.dart';
 
 class MiddleButons extends StatefulWidget {
   const MiddleButons({super.key});
@@ -11,6 +15,139 @@ class MiddleButons extends StatefulWidget {
 class _MiddleButonsState extends State<MiddleButons> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(400, 20, 400, 220),
+      child: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(bottom: 30),
+            child: Divider(
+              color: Colors.black,
+              thickness: 1,
+            ),
+          ),
+          Row(
+            children: [
+              const SizedBox(width: 60),
+              SizedBox(
+                width: 250,
+                height: 60,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Lógica para o botão "quartos"
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(23),
+                    ),
+                    backgroundColor: const Color.fromARGB(255, 179, 40, 235),
+                    shadowColor: Colors.black.withOpacity(0.9),
+                    elevation: 13,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(
+                        Icons.favorite,
+                        color: Colors.white,
+                        size: 24,
+                      ),
+                      SizedBox(width: 8),
+                      Text(
+                        'Quartos favoritos',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(width: 100),
+              SizedBox(
+                width: 250,
+                height: 60,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Lógica para o botão "reservas"
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(23),
+                    ),
+                    backgroundColor: const Color.fromARGB(255, 179, 40, 235),
+                    shadowColor: Colors.black.withOpacity(0.9),
+                    elevation: 13,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(
+                        Icons.calendar_today_outlined,
+                        color: Colors.white,
+                        size: 24,
+                      ),
+                      SizedBox(width: 8),
+                      Text(
+                        'Reservas agendadas',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(width: 100),
+              SizedBox(
+                width: 250,
+                height: 60,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Lógica para o botão "visto recentemente"
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(23),
+                    ),
+                    backgroundColor: const Color.fromARGB(255, 179, 40, 235),
+                    shadowColor: Colors.black.withOpacity(0.9),
+                    elevation: 15,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(
+                        Icons.history,
+                        color: Colors.white,
+                        size: 24,
+                      ),
+                      SizedBox(width: 8),
+                      Text(
+                        'Visto Recentemente',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+          const Padding(
+            padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
+            child: Divider(
+              color: Colors.black,
+              thickness: 1,
+            ),
+          ),
+          const NewsletterComponent(),
+        ],
+      ),
+    );
   }
 }
